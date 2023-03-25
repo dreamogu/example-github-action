81afcf5cc5fa2c038f5e567369828c591d6b3d89
드림오구 <121333344+dreamogu@users.noreply.github.com>
2023-03-25 21:10:19 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -0,0 +1,10 @@
+const solution = (cipher, code) => {
+  let result = '';
+  for (let i = 0; i < cipher.length; i++) {
+    if ((i + 1) % code === 0) {
+      result += cipher[i];
+    }
+  }
+  
+  return result;
+};
